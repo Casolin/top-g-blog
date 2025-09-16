a5a5eb6266991c8b14deb38f98d89722a002dc59
Casolin <animecaster@gmail.com>
2025-09-16 16:41:30 +0100

    gta

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ let hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
+  let closeBtn = document.createElement("div");
+  closeBtn.textContent = "☰";
+  closeBtn.classList.add("closeBtn");
+  menu.appendChild(closeBtn);
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+    closeBtn.remove();
+  });
 });
